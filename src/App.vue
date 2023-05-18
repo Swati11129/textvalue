d766cd3c1aaf3cd07cc5ddc3063906e2a981c6e0
Swati <swati11129@gmail.com>
2023-05-18 11:53:07 +0530

    TextValue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <TextValue v-bind:propsTextValueData="textValueData" v-on:valueClicked="emitEvent($event)" />
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import TextValue from './components/TextValue.vue';
+export default {
+  name: 'App',
+  components:{
+    TextValue
+  },
+   data() {
+    return {
+      textValueData: {
+        value: '1,838',
+        text: 'Low coverage: Under 75% code coverage',
+        color: '',
+        clickable:true,
+        tooltip:true,
+      }
+    };
+  },
+  methods:{
+    emitEvent(capturedValue){
+      console.log(capturedValue);
     }
   }
-}
-</style>
+  
+};
+</script>
---
+++ src/components/TextValue.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container>
+    <div
+      style="font-size: 30px; font-weight: 700; cursor: pointer"
+      :style="colorValueProps"
+      @click="valueClickable"
+    >
+      {{ propsTextValueData.value }}
+    </div>
+    <div class="d-flex">
+      <div style="font-size: 16px">{{ propsTextValueData.text }}</div>
+        <div v-if="propsTextValueData.tooltip">
+          <v-icon >mdi-alpha-i-circle</v-icon>
+        </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "TextValue",
+  props: {
+    propsTextValueData: {
+      type: Object,
+    },
+  },
+  computed: {
+    colorValueProps() {
+      const valueColor = {
+        color: this.propsTextValueData.color,
+      };
+      if (!this.propsTextValueData.color && this.propsTextValueData.clickable) {
+        valueColor.color = "#0000EE";
+      }
+      return valueColor;
+    },
+  },
+  methods: {
+    valueClickable(){
+      if(this.propsTextValueData.clickable){
+        this.$emit('valueClicked',this.propsTextValueData.value)
+      }
+    }
+  },
+};
+</script>
+<style>
+.group {
+  display: flex;
+  flex: 1;
+  justify-content: space-around;
+}
+</style>
